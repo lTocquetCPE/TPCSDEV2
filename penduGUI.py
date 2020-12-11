837557fba2c36f3bdabdea03c39de74a9ddb4bae
PyrrhaDev <loic.tocquet@gmail.com>
2020-12-11 11:34:36 +0100

    added the github link as requested by the teachers

--- penduGUI.py
+++ penduGUI.py
@@ -3,6 +3,7 @@ Project for CPE LYON, CSDEV TP02
 
 11/12/20
 by Loic (Pyrrha) TOCQUET
+https://github.com/lTocquetCPE/TPCSDEV2
 """
 
 from func.GUIIntegration import startGame
--- penduTerminal.py
+++ penduTerminal.py
@@ -3,6 +3,7 @@ Project for CPE LYON, CSDEV TP02
 
 27/09/2020
 by Loïc (Pyrrha) TOCQUET
+https://github.com/lTocquetCPE/TPCSDEV2
 """
 
 from func.TerminalIntegration import startGame
